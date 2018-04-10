e98b008f9092c867ed54746dcea61cd294073d01
Hugo <hugochen1212@gmail.com>
2018-04-09 23:56:53 -0700

    add auth_action file

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,9 +1,34 @@
 import { AsyncStorage } from 'react-native';
-import { FACEBOOK_LOGIN_SUCCESS } from '.types';
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from '.types';
+import { Facebook } from 'expo';
 
+//How to use AsyncStorage:
 //AsyncStorage.setItem('fb_token', token);
 //AsyncStorage.getItem('fb_token';
 
-export const facebookLogin = () => {
+export const facebookLogin = () => async dispatch => {
+ let token = await AsyncStorage.getItem('fb_token');
 
+ if (token) {
+  // Dispatch an action saying FB login is done
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  }
+  else {
+    // Start up B Login process
+    doFacebookLogin(dispatch);
+  }
 };
+
+  const doFacebookLogin = async dispatch =>{
+    let {type, token} = await Facebook.logInWithReadPermissionsAsync('369435900223971', {
+      permisstions: ['public_profile']
+    });
+
+    if (type ==='cancel'){
+      return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+    }
+      await AsyncStorage.setItem('fb_token', token);
+      dispatch({
+        type: FACEBOOK_LOGIN_SUCCESS, payload: token
+      });
+  };
