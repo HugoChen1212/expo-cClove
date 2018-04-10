51d0954c39d4f8a3d7be0656fd8664c8d9f27ab0
Hugo <hugochen1212@gmail.com>
2018-04-09 23:03:29 -0700

    add reducer and store files

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainApp  from './config/index';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store={store}>
       <MainApp />
+      </Provider>
     );
   }
 }
