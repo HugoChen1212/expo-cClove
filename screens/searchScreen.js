7d660c2868db1f1cf21e7daba8db6aabf7362447
Hugo <hugochen1212@gmail.com>
2018-04-11 03:14:38 -0700

    add link to list pet page

--- screens/searchScreen.js
+++ screens/searchScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button} from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 import axios from 'axios';
 import { Dropdown } from 'react-native-material-dropdown';
 
@@ -24,9 +24,11 @@ constructor(props){
      }).catch(error => {
         console.log(error);
      });
-
   }
 
+ onListHandeler = () => {
+    this.props.navigation.navigate('List');
+  }
 
   render() {
 
@@ -66,6 +68,7 @@ constructor(props){
 
     return (
       <View style={styles.formContainer}>
+
         <View style={styles.locationContainer}>
           <Text style={styles.locationTitle}>Find Nearst Shelter</Text>
           <TextInput textAlign="center"
@@ -74,24 +77,35 @@ constructor(props){
           maxLength={5}
           placeholder="Enter You Zipcode" onChangeText={(location) => this.setState({location})} />
           </View>
+
+            <View style={styles.dropdownStyle}>
             <Dropdown
             label='Select Animal'
             data={animalData}
             onChangeText={(value, index, data) => this.setState({animal:value})}
-          />
-          <Dropdown
+            />
+            </View>
+
+          <View style={styles.dropdownStyle}>
+           <Dropdown
             label='Select Age'
             data={ageData}
             onChangeText={(value, index, data) => this.setState({age:value})}
-          />
+            />
+            </View>
+
+           <View style={styles.dropdownStyle}>
            <Dropdown
             label='Select Sex'
             data={sexData}
             onChangeText={(value, index, data) => this.setState({sex:value})}
             />
-            <View style={styles.buttonStyle}>
-              <Button title="Submit" color="#74F363" onPress={() => this.dogSearcher()}/>
             </View>
+
+            <View style={styles.buttonStyle}>
+              <Button title="Submit" color="#74F363" onPress={() => this.dogSearcher()} />
+            </View>
+
             <View style={styles.buttonStyle}>
                 <Button title="View Favorites" color="#74F363" onPress={() => Actions.favorites()}/>
               </View>
@@ -123,6 +137,10 @@ const styles = {
     marginRight: '20%',
     borderRadius: 5,
     marginTop: '2%'
+  },
+  dropdownStyle: {
+    borderBottomColor: '#e6e6e6',
+    borderBottomWidth: 2
   }
   }
 
