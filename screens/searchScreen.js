99b2415c86cff663039d9c0cdca14a18d4934eec
Hugo <hugochen1212@gmail.com>
2018-04-11 00:50:37 -0700

    able to filter pets

--- screens/searchScreen.js
+++ screens/searchScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Button} from 'react-native';
 import axios from 'axios';
+import { Dropdown } from 'react-native-material-dropdown';
+
+
 
 class SearchScreen extends Component {
 
@@ -8,14 +11,14 @@ constructor(props){
     super(props);
     this.state = {
       animal: 'dog',
-      sex: 'F',
-      location: '94124',
+      sex: '',
+      location: '',
       format: 'json'
     };
 }
 
 
-   componentDidMount() {
+   dogSearcher =() => {
     axios.get('http://api.petfinder.com/pet.find?key=dbeaf4be86fcf7cfe093d67eef6689b7&animal='+this.state.animal+'&location='+this.state.location+'&sex='+this.state.sex+'&format='+this.state.format).then( response => {
       console.log(response);
      }).catch(error => {
@@ -26,13 +29,101 @@ constructor(props){
 
 
   render() {
+
+  let animalData = [{
+    value: 'dog',
+  }, {
+    value: 'cat',
+  }, {
+    value: 'bird'
+  }, {
+    value: 'horse'
+  },{
+    value: 'barnyard'
+  },{
+    value: 'reptile'
+  }
+  ];
+
+
+  let ageData = [{
+    value: 'Baby',
+  }, {
+    value: 'Young',
+  }, {
+    value: 'Adult'
+  }, {
+    value: 'Senior'
+  }];
+
+  let sexData = [{
+    value: 'F',
+  }, {
+    value: 'M'
+  }];
+
+
+
     return (
-      <View>
-          <Text>Find Nearst Shelter</Text>
-          <TextInput placeholder="Enter You Zipcode" />
+      <View style={styles.formContainer}>
+        <View style={styles.locationContainer}>
+          <Text style={styles.locationTitle}>Find Nearst Shelter</Text>
+          <TextInput textAlign="center"
+          selectionColor="#B8B8C4"
+          animationDuration={5}
+          maxLength={5}
+          placeholder="Enter You Zipcode" onChangeText={(location) => this.setState({location})} />
+          </View>
+            <Dropdown
+            label='Select Animal'
+            data={animalData}
+            onChangeText={(value, index, data) => this.setState({animal:value})}
+          />
+          <Dropdown
+            label='Select Age'
+            data={ageData}
+            onChangeText={(value, index, data) => this.setState({age:value})}
+          />
+           <Dropdown
+            label='Select Sex'
+            data={sexData}
+            onChangeText={(value, index, data) => this.setState({sex:value})}
+            />
+            <View style={styles.buttonStyle}>
+              <Button title="Submit" color="#74F363" onPress={() => this.dogSearcher()}/>
+            </View>
+            <View style={styles.buttonStyle}>
+                <Button title="View Favorites" color="#74F363" onPress={() => Actions.favorites()}/>
+              </View>
       </View>
       )
   }
 }
+
+
+const styles = {
+  formContainer: {
+    backgroundColor: 'white',
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+  locationContainer: {
+    marginTop: 100,
+    alignItems: 'center'
+  },
+  locationTitle: {
+    fontSize: 18,
+    color: '#BBB193'
+  },
+  buttonStyle: {
+    borderWidth: 2,
+    borderColor: '#F3E263',
+    marginLeft: '20%',
+    marginRight: '20%',
+    borderRadius: 5,
+    marginTop: '2%'
+  }
+  }
 
 export default SearchScreen;
