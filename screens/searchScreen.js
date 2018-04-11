5a28f557025ec060b77b78a9ab61de56cd87ae29
Hugo <hugochen1212@gmail.com>
2018-04-10 23:35:06 -0700

    able to pass the api data

--- screens/searchScreen.js
+++ screens/searchScreen.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput } from 'react-native';
+import axios from 'axios';
 
 class SearchScreen extends Component {
 
 constructor(props){
     super(props);
     this.state = {
-      size: 'L',
-      age: 'Young',
+      animal: 'dog',
       sex: 'F',
-      results: [],
-      location: '94131',
+      location: '94124',
+      format: 'json'
     };
 }
+
+
+   componentDidMount() {
+    axios.get('http://api.petfinder.com/pet.find?key=dbeaf4be86fcf7cfe093d67eef6689b7&animal='+this.state.animal+'&location='+this.state.location+'&sex='+this.state.sex+'&format='+this.state.format).then( response => {
+      console.log(response);
+     }).catch(error => {
+        console.log(error);
+     });
+
+  }
 
 
   render() {
