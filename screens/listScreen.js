0247448ec0cefb1c63e8decee7330bf161a97604
Hugo <hugochen1212@gmail.com>
2018-04-12 01:04:49 -0700

    update datcclove  list file

--- screens/listScreen.js
+++ screens/listScreen.js
@@ -4,17 +4,24 @@ import { Card, Button } from 'react-native-elements';
 
 class ListScreen extends Component {
 
+  renderCard(pet) {
+    console.log{pet.age}
+  }
+
+  eachPet() {
+     return this.props.navigation.state.params.results.pet.map( pet =>{
+        return this.renderCard(pet);
+     });
+  }
 
   render() {
 
-    let data = this.props.navigation.state.params;
-    //console.log(data);
+   let pets = this.props.navigation.state.params.results.pet;
+    console.log(pets[0].age);
 
     return (
       <View>
-        <Text>ListScreen</Text>
-        <Text>ListScreen</Text>
-        <Text>ListScreen</Text>
+        {this.eachPet()}
       </View>
       )
   }
