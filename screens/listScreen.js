af942488e8193cd32e9bd5a819ff094e18f7e102
Hugo <hugochen1212@gmail.com>
2018-04-12 23:22:47 -0700

    able to passing data into card elements

--- screens/listScreen.js
+++ screens/listScreen.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Card, Button } from 'react-native-elements';
+import MyCard from '../components/card'
+
+
 
 class ListScreen extends Component {
 
-  renderCard(pet) {
-    console.log{pet.age}
+
+    renderPet(pet, index) {
+
+      let petName = pet.name.$t;
+      let petAge = pet.age.$t;
+      let petSex = pet.sex.$t;
+      let petSize = pet.size.$t;
+
+      let Media = pet.media;
+      let petUri = JSON.stringify(Media)!=='{}' ?
+         pet.media.photos.photo[2].$t :
+        'http://photos.petfinder.com/photos/pets/33754516/1/?bust=1447200763&width=500&-x.jpg'
+
+    return (
+      <View key={index}>
+        <Card
+          title={petName}
+          image={{ uri:petUri }}
+        >
+        <View style={{flexDirection: 'row'}}>
+          <Text>Age: {petAge}</Text>
+          <Text>Sex: {petSex}</Text>
+          <Text>Size: {petSize}</Text>
+          </View>
+        </Card>
+      </View>
+      );
   }
 
-  eachPet() {
-     return this.props.navigation.state.params.results.pet.map( pet =>{
-        return this.renderCard(pet);
-     });
-  }
+
+
+
 
   render() {
 
    let pets = this.props.navigation.state.params.results.pet;
-    console.log(pets[0].age);
+   // console.log(pets);
+   //  console.log(pets[1].media.photos.photo[2].$t);
+
 
     return (
-      <View>
-        {this.eachPet()}
-      </View>
+      <ScrollView>
+       <MyCard pets={pets}
+        renderPet={this.renderPet}
+        />
+      </ScrollView>
       )
+
   }
 }
 
--- screens/searchScreen.js
+++ screens/searchScreen.js
@@ -19,6 +19,8 @@ constructor(props){
 }
 
 
+
+
    dogSearcher =() => {
     axios.get('http://api.petfinder.com/pet.find?key=dbeaf4be86fcf7cfe093d67eef6689b7&animal='+this.state.animal+'&location='+this.state.location+'&sex='+this.state.sex+'&format='+this.state.format).then( response => {
       this.setState({
